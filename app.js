fda31d729673cbe9aa3ff8a40d384bcf3d240a33
mishkamebonia <60870999+mishkamebonia@users.noreply.github.com>
2023-04-17 17:16:50 +0400

    dark theme

---
+++ app.js
@@ -0,0 +1,28 @@
+const body = document.body
+const nav = document.querySelector("nav")
+const footer = document.querySelector("footer")
+const logo = document.querySelector(".logo")
+const themeBtn = document.querySelector("#theme")
+const author = document.querySelector(".author")
+const header = document.querySelectorAll("h1")
+const description = document.querySelectorAll(".info")
+const btn = document.querySelectorAll(".link-btn")
+
+const changeTheme = () => {
+  nav.classList.toggle("dark-div-background")
+  footer.classList.toggle("dark-div-background")
+  body.classList.toggle("dark-background")
+  logo.classList.toggle("dark-color")
+  themeBtn.classList.toggle("dark-btn")
+  author.classList.toggle("dark-color")
+  
+  header.forEach((i) => {
+    i.classList.toggle("dark-color")
+  })
+  description.forEach((i) => {
+    i.classList.toggle("dark-color")
+  })
+  btn.forEach((i) => {
+    i.classList.toggle("dark-btn")
+  })
+} 
